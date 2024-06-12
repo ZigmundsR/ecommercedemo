ea755e1ba0c89ea695a085186a684c5f31d1f24c
ZigmundsR <78530210+ZigmundsR@users.noreply.github.com>
2024-06-12 15:41:12 +0300

    Created database connection and REST CRUD functionality
    also removed unnecessary HTTP methods

---
+++ src/main/java/com/zigmunds/rieksts/ecommercedemo/dao/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.zigmunds.rieksts.ecommercedemo.dao;
+
+import com.zigmunds.rieksts.ecommercedemo.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
