d71fdc9ee6d2aed954afa2b0d25eed3899207f23
ZigmundsR <78530210+ZigmundsR@users.noreply.github.com>
2024-06-27 13:45:01 +0300

    Created search for Products by Keyword

--- js-storefront/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ js-storefront/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -14,17 +14,43 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
   currentCategoryId: number = 1;
+  searchMode: boolean = false;
 
   constructor(private productService: ProductService,
-              private route: ActivatedRoute) { }
+    private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.route.paramMap.subscribe(() => {
       this.listProducts();
     });
   }
 
   listProducts() {
+
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    }
+    else {
+      this.handleListProducts();
+    }
+
+  }
+
+  handleSearchProducts() {
+
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    // now search for the products using keyword
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
+
+  handleListProducts() {
 
     // check if "id" parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
@@ -34,6 +60,7 @@ export class ProductListComponent implements OnInit {
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
     }
     else {
+      // category id not available ... default to category id 1
       this.currentCategoryId = 1;
     }
 
@@ -44,5 +71,4 @@ export class ProductListComponent implements OnInit {
       }
     )
   }
-
 }
