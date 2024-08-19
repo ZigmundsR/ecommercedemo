13b140022479a9c00b37f5844d8b78a7b2540df4
ZigmundsR <78530210+ZigmundsR@users.noreply.github.com>
2024-08-19 19:18:24 +0300

    Created another environment and added api url property

--- js-storefront/angular-ecommerce/src/app/services/shop-form.service.ts
+++ js-storefront/angular-ecommerce/src/app/services/shop-form.service.ts
@@ -4,14 +4,15 @@ import { Observable, of } from 'rxjs';
 import { State } from '../common/state';
 import { Country } from '../common/country';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopFormService {
 
-  private countriesUrl = 'http://localhost:8080/api/countries';
-  private statesUrl = 'http://localhost:8080/api/states';
+  private countriesUrl = environment.apiUrl +'/countries';
+  private statesUrl = environment.apiUrl +'/states';
 
   constructor(private httpClient: HttpClient) { }
 
